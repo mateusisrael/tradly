8b340f6d6d16e1816caa424b7d4c9ae874d9d6c6
mateusisrael <mateuss.i.fernandes@gmail.com>
2022-03-05 16:47:31 -0300

    componentize search bar

---
+++ lib/components/search_bar.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+
+class SearchBar extends StatefulWidget {
+  const SearchBar({Key? key}) : super(key: key);
+
+  @override
+  State<SearchBar> createState() => _SearchBarState();
+}
+
+class _SearchBarState extends State<SearchBar> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.only(left: 16, right: 16),
+      height: 46,
+      decoration: const BoxDecoration(
+        boxShadow: [
+          BoxShadow(
+              color: Color.fromRGBO(58, 76, 130, 0.0722656),
+              blurRadius: 38,
+              offset: Offset(0, 19))
+        ],
+        borderRadius: BorderRadius.all(Radius.circular(25)),
+        color: Colors.white,
+      ),
+      child: Row(
+        children: const [
+          Icon(
+            Icons.search,
+            color: Color(0xff33907C),
+            size: 24,
+          ),
+          Expanded(
+              child: Padding(
+            padding: EdgeInsets.only(left: 22),
+            child: TextField(
+              decoration: InputDecoration(
+                  hintStyle: TextStyle(
+                    color: Color.fromRGBO(79, 79, 79, 0.2),
+                    fontSize: 14,
+                  ),
+                  hintText: 'Search campaign',
+                  border: InputBorder.none),
+            ),
+          ))
+        ],
+      ),
+    );
+  }
+}
--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:material_tradly/components/search_bar.dart';
 
 class Home extends StatelessWidget {
   const Home({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Stack(
+    return Column(
       children: [SubStatusBar()],
     );
   }
@@ -16,7 +17,7 @@ class SubStatusBar extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final primaryColor = Theme.of(context).primaryColor;
+    // final primaryColor = Theme.of(context).primaryColor;
     return Container(
         padding: const EdgeInsets.only(
           left: 16,
@@ -27,37 +28,6 @@ class SubStatusBar extends StatelessWidget {
         decoration: BoxDecoration(
           color: Theme.of(context).primaryColor,
         ),
-        child: Column(children: [
-          Container(
-            padding: EdgeInsets.only(left: 16, right: 16),
-            height: 46,
-            decoration: const BoxDecoration(
-              borderRadius: BorderRadius.all(Radius.circular(25)),
-              color: Colors.white,
-            ),
-            child: Row(
-              children: const [
-                Icon(
-                  Icons.search,
-                  color: Color(0xff33907C),
-                  size: 24,
-                ),
-                Expanded(
-                    child: Padding(
-                  padding: EdgeInsets.only(left: 22),
-                  child: TextField(
-                    decoration: InputDecoration(
-                        hintStyle: TextStyle(
-                          color: Color.fromRGBO(79, 79, 79, 0.2),
-                          fontSize: 14,
-                        ),
-                        hintText: 'Search campaign',
-                        border: InputBorder.none),
-                  ),
-                ))
-              ],
-            ),
-          ),
-        ]));
+        child: Column(children: [SearchBar()]));
   }
 }
