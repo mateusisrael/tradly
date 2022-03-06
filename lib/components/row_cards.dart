4c7324a1df15124172b271a0d8e853ce01a9b107
mateusisrael <mateuss.i.fernandes@gmail.com>
2022-03-05 22:09:13 -0300

    adjust cards spacement

--- lib/components/row_cards.dart
+++ lib/components/row_cards.dart
@@ -6,33 +6,51 @@ class CardsScrow extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      padding: const EdgeInsets.only(top: 11, bottom: 11),
+      margin: const EdgeInsets.symmetric(vertical: 11),
+      padding: const EdgeInsets.only(left: 9, right: 9),
       height: 165,
       child: ListView(
         scrollDirection: Axis.horizontal,
         children: [
           Container(
-            margin: EdgeInsets.symmetric(horizontal: 20),
+            margin: EdgeInsets.symmetric(horizontal: 7),
             width: 302,
-            decoration: BoxDecoration(
-              image: const DecorationImage(
-                fit: BoxFit.fitWidth,
-                alignment: Alignment.topLeft,
-                image: AssetImage('images/card1.jpeg'),
-              ),
-              color: Colors.deepOrange[200],
+            decoration: const BoxDecoration(
+              borderRadius: BorderRadius.all(Radius.circular(10)),
+              image: DecorationImage(
+                  fit: BoxFit.fitWidth,
+                  alignment: Alignment.topLeft,
+                  image: AssetImage('images/card1.jpeg'),
+                  opacity: 0.6),
+              color: Colors.black,
             ),
           ),
           Container(
-            margin: EdgeInsets.symmetric(horizontal: 20),
+            margin: EdgeInsets.symmetric(horizontal: 7),
             width: 302,
-            color: Colors.brown[200],
+            decoration: const BoxDecoration(
+              borderRadius: BorderRadius.all(Radius.circular(10)),
+              image: DecorationImage(
+                  fit: BoxFit.fitWidth,
+                  alignment: Alignment.topLeft,
+                  image: AssetImage('images/card1.jpeg'),
+                  opacity: 0.6),
+              color: Colors.black,
+            ),
           ),
           Container(
-            margin: EdgeInsets.symmetric(horizontal: 20),
+            margin: EdgeInsets.symmetric(horizontal: 7),
             width: 302,
-            color: Colors.green[200],
-          )
+            decoration: const BoxDecoration(
+              borderRadius: BorderRadius.all(Radius.circular(10)),
+              image: DecorationImage(
+                  fit: BoxFit.fitWidth,
+                  alignment: Alignment.topLeft,
+                  image: AssetImage('images/card1.jpeg'),
+                  opacity: 0.6),
+              color: Colors.black,
+            ),
+          ),
         ],
       ),
     );
