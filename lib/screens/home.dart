3381208973cf29219e645ad191a2391fd4eb8720
mateusisrael <mateuss.i.fernandes@gmail.com>
2022-03-12 21:35:31 -0300

    add images in the grid

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -5,6 +5,29 @@ import 'package:material_tradly/components/search_bar.dart';
 class Home extends StatelessWidget {
   const Home({Key? key}) : super(key: key);
 
+  _gridContent(String imagePath, String title) {
+    return Container(
+      decoration: BoxDecoration(
+          color: const Color(0xff000000),
+          image: DecorationImage(
+              opacity: 0.7,
+              fit: BoxFit.cover,
+              alignment: Alignment.center,
+              image: AssetImage(imagePath))),
+      width: 90,
+      height: 90,
+      child: Center(
+        child: Text(
+          title,
+          style: const TextStyle(
+              fontSize: 11,
+              fontWeight: FontWeight.w600,
+              color: Color(0xffffffff)),
+        ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -19,52 +42,14 @@ class Home extends StatelessWidget {
           mainAxisSpacing: 1,
           crossAxisCount: 4,
           children: [
-            Container(
-              decoration: BoxDecoration(
-                  color: Colors.green[200],
-                  image: const DecorationImage(
-                      image: AssetImage('images/beverages.jpeg'))),
-              width: 90,
-              height: 90,
-            ),
-            Container(
-              decoration: BoxDecoration(
-                  color: Colors.green[200],
-                  image: const DecorationImage(
-                      image: AssetImage('images/bread-and-bakery.jpeg'))),
-              width: 90,
-              height: 90,
-            ),
-            Container(
-              decoration: BoxDecoration(color: Colors.green[200]),
-              width: 90,
-              height: 90,
-            ),
-            Container(
-              decoration: BoxDecoration(color: Colors.green[200]),
-              width: 90,
-              height: 90,
-            ),
-            Container(
-              decoration: BoxDecoration(color: Colors.green[200]),
-              width: 90,
-              height: 90,
-            ),
-            Container(
-              decoration: BoxDecoration(color: Colors.green[200]),
-              width: 90,
-              height: 90,
-            ),
-            Container(
-              decoration: BoxDecoration(color: Colors.green[200]),
-              width: 90,
-              height: 90,
-            ),
-            Container(
-              decoration: BoxDecoration(color: Colors.green[200]),
-              width: 90,
-              height: 90,
-            ),
+            _gridContent('images/beverages.jpeg', 'Beverages'),
+            _gridContent('images/bread-and-bakery.jpeg', 'Bread & Bakery'),
+            _gridContent('images/vegetables.jpeg', 'Vegetables'),
+            _gridContent('images/fruit.jpeg', 'Fruit'),
+            _gridContent('images/egg.jpeg', 'Egg'),
+            _gridContent('images/frozen-veg.jpeg', 'Frozen veg'),
+            _gridContent('images/home-care.jpeg', 'Homecare'),
+            _gridContent('images/pet-care.jpeg', 'Pet Care'),
           ],
         )
       ],
