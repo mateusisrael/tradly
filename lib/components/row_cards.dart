948dd5aa80ea165b09032a6d6f49ff257f298b08
mateusisrael <mateuss.i.fernandes@gmail.com>
2022-04-12 23:17:00 -0300

    adjust cards row in tablet

--- lib/components/row_cards.dart
+++ lib/components/row_cards.dart
@@ -5,92 +5,103 @@ class CardsScrow extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      margin: const EdgeInsets.only(top: 11, bottom: 16),
-      // padding: const EdgeInsets.only(left: 9, right: 9),
-      height: 165,
-      child: ListView(
-        scrollDirection: Axis.horizontal,
-        children: [
-          Container(
-            margin: const EdgeInsets.only(left: 16, right: 7),
-            width: 302,
-            decoration: const BoxDecoration(
-              borderRadius: BorderRadius.all(Radius.circular(10)),
-              image: DecorationImage(
-                  fit: BoxFit.fitWidth,
-                  alignment: Alignment.topLeft,
-                  image: AssetImage('images/card1.jpeg'),
-                  opacity: 0.6),
-              color: Colors.black,
-            ),
-            child: Padding(
-              padding: const EdgeInsets.only(top: 51, left: 17, right: 80),
-              child: Column(
-                children: [
-                  Text('Ready to deliver to your home'.toUpperCase(),
-                      style: const TextStyle(
-                          fontSize: 14,
-                          fontWeight: FontWeight.w600,
-                          letterSpacing: 1.2,
-                          color: Colors.white))
-                ],
+    return AspectRatio(
+      aspectRatio: 1.83,
+      child: Container(
+        margin: const EdgeInsets.only(top: 11, bottom: 16),
+        // padding: const EdgeInsets.only(left: 9, right: 9),
+        child: ListView(
+          scrollDirection: Axis.horizontal,
+          children: [
+            AspectRatio(
+              aspectRatio: 1.83 / 1,
+              child: Container(
+                margin: const EdgeInsets.only(left: 16, right: 7),
+                width: 302,
+                decoration: const BoxDecoration(
+                  borderRadius: BorderRadius.all(Radius.circular(10)),
+                  image: DecorationImage(
+                      fit: BoxFit.fitWidth,
+                      alignment: Alignment.topLeft,
+                      image: AssetImage('images/card1.jpeg'),
+                      opacity: 0.6),
+                  color: Colors.black,
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.only(top: 51, left: 17, right: 80),
+                  child: Column(
+                    children: [
+                      Text('Ready to deliver to your home'.toUpperCase(),
+                          style: const TextStyle(
+                              fontSize: 14,
+                              fontWeight: FontWeight.w600,
+                              letterSpacing: 1.2,
+                              color: Colors.white))
+                    ],
+                  ),
+                ),
               ),
             ),
-          ),
-          Container(
-            margin: const EdgeInsets.symmetric(horizontal: 7),
-            width: 302,
-            decoration: const BoxDecoration(
-              borderRadius: BorderRadius.all(Radius.circular(10)),
-              image: DecorationImage(
-                  fit: BoxFit.fitWidth,
-                  alignment: Alignment.topLeft,
-                  image: AssetImage('images/comer-saudavel.jpg'),
-                  opacity: 0.6),
-              color: Colors.black,
-            ),
-            child: Padding(
-              padding: const EdgeInsets.only(top: 51, left: 17, right: 80),
-              child: Column(
-                children: [
-                  Text('The benefits of healthy eating'.toUpperCase(),
-                      style: const TextStyle(
-                          fontSize: 14,
-                          fontWeight: FontWeight.w600,
-                          letterSpacing: 1.2,
-                          color: Colors.white))
-                ],
+            AspectRatio(
+              aspectRatio: 1.83 / 1,
+              child: Container(
+                margin: const EdgeInsets.symmetric(horizontal: 7),
+                width: 302,
+                decoration: const BoxDecoration(
+                  borderRadius: BorderRadius.all(Radius.circular(10)),
+                  image: DecorationImage(
+                      fit: BoxFit.fitWidth,
+                      alignment: Alignment.topLeft,
+                      image: AssetImage('images/comer-saudavel.jpg'),
+                      opacity: 0.6),
+                  color: Colors.black,
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.only(top: 51, left: 17, right: 80),
+                  child: Column(
+                    children: [
+                      Text('The benefits of healthy eating'.toUpperCase(),
+                          style: const TextStyle(
+                              fontSize: 14,
+                              fontWeight: FontWeight.w600,
+                              letterSpacing: 1.2,
+                              color: Colors.white))
+                    ],
+                  ),
+                ),
               ),
             ),
-          ),
-          Container(
-            margin: const EdgeInsets.only(left: 7, right: 16),
-            width: 302,
-            decoration: const BoxDecoration(
-              borderRadius: BorderRadius.all(Radius.circular(10)),
-              image: DecorationImage(
-                  fit: BoxFit.fitWidth,
-                  alignment: Alignment.topLeft,
-                  image: AssetImage('images/comer-saudavel2.jpg'),
-                  opacity: 0.6),
-              color: Colors.black,
-            ),
-            child: Padding(
-              padding: const EdgeInsets.only(top: 51, left: 17, right: 80),
-              child: Column(
-                children: [
-                  Text('Fresh fruits and vegetables'.toUpperCase(),
-                      style: const TextStyle(
-                          fontSize: 14,
-                          fontWeight: FontWeight.w600,
-                          letterSpacing: 1.2,
-                          color: Colors.white))
-                ],
+            AspectRatio(
+              aspectRatio: 1.83 / 1,
+              child: Container(
+                margin: const EdgeInsets.only(left: 7, right: 16),
+                width: 302,
+                decoration: const BoxDecoration(
+                  borderRadius: BorderRadius.all(Radius.circular(10)),
+                  image: DecorationImage(
+                      fit: BoxFit.fitWidth,
+                      alignment: Alignment.topLeft,
+                      image: AssetImage('images/comer-saudavel2.jpg'),
+                      opacity: 0.6),
+                  color: Colors.black,
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.only(top: 51, left: 17, right: 80),
+                  child: Column(
+                    children: [
+                      Text('Fresh fruits and vegetables'.toUpperCase(),
+                          style: const TextStyle(
+                              fontSize: 14,
+                              fontWeight: FontWeight.w600,
+                              letterSpacing: 1.2,
+                              color: Colors.white))
+                    ],
+                  ),
+                ),
               ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
